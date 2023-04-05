bac94dd7dc16a5910f1a217d4366955ddd45f153
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-05 19:44:32 +0300

    updated script

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ console.log(users[0].username)
 const container = document.getElementById("container")
 
 const usersHtml = users.map((user) => {
-    return `<div class="user-profile">${user.username}</div>`
+    return `
+    <div class="user-profile">
+        <div class="image"></div>
+        <p>${user.username}</p>
+        <p>${user.firstName} ${user.lastName}</p>
+        <p>${user.gender}</p>
+        <p>${user.email}</p>
+    </div>`
 }).join("")
 
 container.innerHTML = usersHtml
