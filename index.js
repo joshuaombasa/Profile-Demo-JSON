75c7e15eaf6e52bd4e537515b9f1f5c66bccbd7b
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-05 19:31:56 +0300

    updated script

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import users from './data.js'
 
+
+console.log(users[0].username)
+
 const container = document.getElementById("container")
 
 const usersHtml = users.map((user) => {
-    return `<div class="user-profile">${user}</div>`
-})
+    return `<div class="user-profile">${user.username}</div>`
+}).join("")
 
 container.innerHTML = usersHtml
